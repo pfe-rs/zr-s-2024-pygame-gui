bf8c5c3476992a40487f89947bd99020bd68d021
Sugaravel <anjavujanacra@gmail.com>
2024-03-28 21:02:30 +0100

    Klasa prozor

--- Klase.py
+++ Klase.py
@@ -1,22 +1,23 @@
 import pygame
 class Lajsna:
-    def __init__(self,x,y,w,h):
+    def __init__(self,x,y,width,height):
         self.x=x
         self.y=y
-        self.w=w
-        self.h=h
-        self.xx=self.x+self.w*14/15-self.w/30
-        self.xy=self.y+self.h/10
-        self.xw=self.w/15
-        self.xh=self.h*8/10
-        self.prevpress=[-1,-1]
+        self.width=width
+        self.height=height
+        # self.xx=self.x+self.width*14/15-self.width/30
+        # self.xy=self.y+self.height/10
+        # self.xwidth=self.width/15
+        # self.xheight=self.height*8/10
         self.press=2
+        self.prevpress=[-1,-1]
     def draw(self,screen):
-        pygame.draw.rect(screen,(0, 102, 255),pygame.Rect(self.x,self.y,self.w,self.h))
-        pygame.draw.line(screen,(200, 20, 0),(self.xx,self.xy),(self.xx+self.xw,self.xy+self.xh),3)
-        pygame.draw.line(screen,(200, 20, 0),(self.xx,self.xy+self.xh),(self.xx+self.xw,self.xy),3)
+        pygame.draw.rect(screen,(0, 102, 255),pygame.Rect(self.x,self.y,self.width,self.height))
+        pygame.draw.line(screen,(200, 20, 0),(self.x+self.width*14/15-self.width/30,self.y+self.height/10),(self.x+self.width*14/15-self.width/30+self.width/15,self.y+self.height/10+self.height*8/10),3)
+        pygame.draw.line(screen,(200, 20, 0),(self.x+self.width*14/15-self.width/30,self.y+self.height/10+self.height*8/10),(self.x+self.width*14/15-self.width/30+self.width/15,self.y+self.height/10),3)
+   
     def click(self,x,y):
-        if x>self.xx and x<self.xx+self.xw:
+        if x>self.x+self.width*14/15-self.width/30 and x<self.x+self.width*14/15-self.width/30+self.width/15:
             self.press=0
         else:
             self.press=1
@@ -24,28 +25,23 @@ class Lajsna:
     def drag(self,x,y,prevx,prevy):
         self.x+=x
         self.y+=y
-        self.xx=self.x+self.w*14/15-self.w/30
-        self.xy=self.y+self.h/10
-        self.xw=self.w/15
-        self.xh=self.h*8/10
         #self.prevpress=[self.x,self.y]
         self.prevpress=[prevx,prevy]
         
 class Prozor:
-    def __init__(self,x,y,w,h,labeltext):
+    def __init__(self,x,y,width,height,labeltext):
         self.x=x
         self.y=y
-        self.w=w
-        self.h=h
+        self.width=width
+        self.height=height
         self.label=labeltext
-        self.lajsna=Lajsna(self.x,self.y,self.w,30)
+        self.lajsna=Lajsna(self.x,self.y,self.width,30)
     def draw(self,screen):
-        pygame.draw.rect(screen,(0, 102, 255),pygame.Rect(self.x,self.y,self.w,self.h))
+        pygame.draw.rect(screen,(153, 153, 102),pygame.Rect(self.x,self.y,self.width,self.height))
         self.lajsna.draw(screen)
-        
-
-
-
+    def click(self,x,y):
+        if(x>=self.lajsna.x and x<=self.lajsna.x+self.lajsna.width) and (y>self.lajsna.y and y<self.lajsna.y+self.lajsna.height):
+            self.lajsna.click(x,y)
 
 
 
---
+++ Projekat.py
@@ -0,0 +1,38 @@
+from Klase import *
+import pygame 
+background_colour = (0, 0, 0) 
+screen = pygame.display.set_mode((1750, 850)) 
+pygame.display.set_caption('GUI') 
+screen.fill(background_colour) 
+pygame.display.flip() 
+running = True
+prozori=[]
+prozori.append(Prozor(450,200,600,300,'a'))
+while running: 
+    for event in pygame.event.get():     
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_u) or event.type==pygame.QUIT:
+            running = False
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            for prozor in prozori:
+                if(x>=prozor.lajsna.x and x<=prozor.lajsna.x+prozor.lajsna.width) and (y>prozor.lajsna.y and y<prozor.lajsna.y+prozor.lajsna.height):
+                    prozor.lajsna.click(x,y)
+                    if prozor.lajsna.press==0:
+                        prozori.remove(prozor)
+                #print(lajsna.press)
+        if event.type==pygame.MOUSEBUTTONUP:
+            for prozor in prozori:
+                prozor.lajsna.press=2
+        if event.type==pygame.MOUSEMOTION:
+            for prozor in prozori:
+                if prozor.lajsna.press==1:
+                    prozor.lajsna.drag(x-prozor.lajsna.prevpress[0],y-prozor.lajsna.prevpress[1],x,y)
+    x, y = pygame.mouse.get_pos()
+    #print(x,y)
+    screen.fill(background_colour) 
+    for prozor in prozori:
+        prozor.draw(screen)
+    pygame.draw.circle(screen,(254,254,254), [x,y], 7)
+    pygame.display.update()
+
+        
+
