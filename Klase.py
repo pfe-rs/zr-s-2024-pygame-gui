fcc81f6d6d2fbeed7795c3412a1a2b638a6fffc9
Sugaravel <anjavujanacra@gmail.com>
2024-03-28 20:34:29 +0100

    Sitne izmene

--- Klase.py
+++ Klase.py
@@ -1,21 +1,21 @@
 import pygame
 class Lajsna:
-    def __init__(self,x,y,w,h):
+    def __init__(self,x,y,width,height):
         self.x=x
         self.y=y
-        self.w=w
-        self.h=h
-        self.xx=self.x+self.w*14/15-self.w/30
-        self.xy=self.y+self.h/10
-        self.xw=self.w/15
-        self.xh=self.h*8/10
+        self.width=width
+        self.height=height
+        # self.xx=self.x+self.width*14/15-self.width/30
+        # self.xy=self.y+self.height/10
+        # self.xwidth=self.width/15
+        # self.xheight=self.height*8/10
     def draw(self,screen):
-        pygame.draw.rect(screen,(0, 102, 255),pygame.Rect(self.x,self.y,self.w,self.h))
-        pygame.draw.line(screen,(200, 20, 0),(self.xx,self.xy),(self.xx+self.xw,self.xy+self.xh),3)
-        pygame.draw.line(screen,(200, 20, 0),(self.xx,self.xy+self.xh),(self.xx+self.xw,self.xy),3)
+        pygame.draw.rect(screen,(0, 102, 255),pygame.Rect(self.x,self.y,self.width,self.height))
+        pygame.draw.line(screen,(200, 20, 0),(self.x+self.width*14/15-self.width/30,self.y+self.height/10),(self.x+self.width*14/15-self.width/30+self.width/15,self.y+self.height/10+self.height*8/10),3)
+        pygame.draw.line(screen,(200, 20, 0),(self.x+self.width*14/15-self.width/30,self.y+self.height/10+self.height*8/10),(self.x+self.width*14/15-self.width/30+self.width/15,self.y+self.height/10),3)
     def click(self,x,y):
-        if(x>=self.x and x<=self.x+self.w) and (y>self.y and y<self.y+self.h):
-            if (x>self.xx and x<self.xx+self.xw):
+        if(x>=self.x and x<=self.x+self.width) and (y>self.y and y<self.y+self.height):
+            if (x>self.x+self.width*14/15-self.width/30 and x<self.x+self.width*14/15-self.width/30+self.width/15):
                 print('na x')
             else: print('na lajsni')
     #def pressed(self,screen):
