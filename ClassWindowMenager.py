893673467839bf9b2ebc0875f5a0a15662cb7cf2
Sugaravel <anjavujanacra@gmail.com>
2024-03-29 10:12:02 +0100

    #25 Implementacija klase WindowMenager i mogucnost da se kontrolisu vise otvorenih prozora

---
+++ ClassWindowMenager.py
@@ -0,0 +1,28 @@
+from ClassProzor import *
+from ClassLajsna import *
+import pygame
+class WindowMenager:
+    def __init__(self):
+        self.prozori=[]
+    def add_prozor(self):
+        self.prozori.append(Prozor(100,50,1500,750,'a'))
+    def remove_prozor(self,prozor):
+        self.prozori.remove(prozor)
+    def draw(self,screen):
+        for prozor in self.prozori:
+            prozor.draw(screen)
+    def check_click(self,x,y):
+        for i in range(len(self.prozori)-1,-1,-1):
+            print(i)
+            if(x>=self.prozori[i].x and x<=self.prozori[i].x+self.prozori[i].width) and (y>self.prozori[i].y and y<self.prozori[i].y+self.prozori[i].height):
+                self.prozori[i].click(x,y)
+                if self.prozori[i].lajsna.press==0:
+                    self.remove_prozor(self.prozori[i])
+                break
+    def mouseup(self):
+        for prozor in self.prozori:
+                prozor.lajsna.press=2
+    def drag(self,x,y):
+        for prozor in self.prozori:
+            if prozor.lajsna.press==1:
+                prozor.drag(x-prozor.lajsna.prevpress[0],y-prozor.lajsna.prevpress[1],x,y)
--- Projekat.py
+++ Projekat.py
@@ -1,3 +1,4 @@
+from ClassWindowMenager import *
 from ClassProzor import *
 from ClassLajsna import *
 import pygame
@@ -8,31 +9,30 @@ screen.fill(background_colour)
 pygame.display.flip() 
 running = True
 prozori=[]
+window_menager=WindowMenager()
 while running: 
+    x, y = pygame.mouse.get_pos()
     for event in pygame.event.get():     
         if (event.type == pygame.KEYDOWN and event.key == pygame.K_u) or event.type==pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
-            prozori.append(Prozor(100,50,1500,750,'a'))
+            window_menager.add_prozor()
         if event.type==pygame.MOUSEBUTTONDOWN:
-            for prozor in prozori:
-                if(x>=prozor.x and x<=prozor.x+prozor.width) and (y>prozor.y and y<prozor.y+prozor.height):
-                    prozor.click(x,y)
-                    if prozor.lajsna.press==0:
-                        prozori.remove(prozor)
+            window_menager.check_click(x,y)
+            # for prozor in prozori:
+            #     if(x>=prozor.x and x<=prozor.x+prozor.width) and (y>prozor.y and y<prozor.y+prozor.height):
+            #         prozor.click(x,y)
+            #         if prozor.lajsna.press==0:
+            #             prozori.remove(prozor)
                 #print(lajsna.press)
         if event.type==pygame.MOUSEBUTTONUP:
-            for prozor in prozori:
-                prozor.lajsna.press=2
+            window_menager.mouseup()
         if event.type==pygame.MOUSEMOTION:
-            for prozor in prozori:
-                if prozor.lajsna.press==1:
-                    prozor.drag(x-prozor.lajsna.prevpress[0],y-prozor.lajsna.prevpress[1],x,y)
-    x, y = pygame.mouse.get_pos()
+            window_menager.drag(x,y)
+    
     #print(x,y)
     screen.fill(background_colour) 
-    for prozor in prozori:
-        prozor.draw(screen)
+    window_menager.draw(screen)
     pygame.draw.circle(screen,(254,254,254), [x,y], 7)
     pygame.display.update()
 
