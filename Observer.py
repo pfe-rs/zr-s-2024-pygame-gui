1d1107d4e8c6095c9878d686b597fac655bda0a2
Sugaravel <anjavujanacra@gmail.com>
2024-03-29 14:59:00 +0100

    Pocetak EventHandlera

---
+++ Observer.py
@@ -0,0 +1,17 @@
+from ClassWindowMenager import *
+from ClassProzor import *
+from ClassLajsna import *
+
+class EventHandler:
+    def __init__(self,WinMenager):
+        self.WinMenager=WinMenager
+        self.closewindow=[WinMenager.remove_prozor]
+    def event_trig(self,event):
+        pass
+
+
+
+
+
+
+
