e8157bc9c64c29ecbca03a3bd2db7d147a3dec59
Sugaravel <anjavujanacra@gmail.com>
2024-03-30 11:47:24 +0100

    Lista labele odakle program bira labelu za prozor

--- Labela.py
+++ Labela.py
@@ -2,20 +2,21 @@ import pygame
 import random
 import string
 
+labele=['Prozor 1','Prozorcic','Prozor 2','Prozor celavi','Prozor 3','Prozor VSK','Prozorko','Window.py','WindowMaster.py',"Observer.py",'Necu da se igram vise.py','Anja Vujanac <3','Pomocnik Bosanac']
+
 class Label(pygame.sprite.Sprite):
     def __init__(self, x, y):
+        global labele
         super().__init__()
         pygame.font.init()
         self.font = pygame.font.SysFont('Arial',15)
         self.x = x
         self.y = y
         self.color = (0,0,0)
-        self.generate_random_text()
+        self.text=random.choice(labele)
         self.image = self.font.render(self.text, True, self.color)
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
-    def generate_random_text(self):
-        self.text = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(1, 10)))
 
     def set_text(self, text):
         self.text = text
